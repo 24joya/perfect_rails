db03bc1be788746364a08eae5c33831bc00e3663
JoyaNinomiya <joya0319p@gmail.com>
2024-02-12 08:11:32 +0900

    feat: bookモデルにsales_statusカラムを追加 #1

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,5 +6,11 @@ class Book < ApplicationRecord
     validates :name, presence: true
     validates :name, length: { maximum: 25 }
     validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+    enum sales_status: {
+        reservation: 0,
+        now_on_sale: 1,
+        end_of_print: 2,
+    }
 end
   
